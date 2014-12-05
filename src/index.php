05a3d1c87cdebf6395b60d31b129c0570a8d41ed
ARCHER Stephane <archer_s@epita.fr>
2014-12-05 04:15:15 +0100

    add the first page

---
+++ src/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Hackaday</title>
+	</head>
+
+	<body>
+		<header>
+			Hackaday
+		</header>
+		<nav>
+			<ul>
+				<li>Home</li>
+			</ul>
+		</nav>
+		<section>
+		</section>
+		<footer>
+		</footer>
+	</body>
+</html>
